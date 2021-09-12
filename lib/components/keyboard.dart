e73e9a90ea5bc00eb597a0557c1f24aa6d636912
kevempedro <kevempereira520@gmail.com>
2021-09-12 10:14:16 -0300

    first commit

---
+++ lib/components/keyboard.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+import 'button.dart';
+import 'button_row.dart';
+
+class Keyboard extends StatelessWidget {
+  final void Function(String) callBack;
+
+  const Keyboard({
+    Key? key,
+    required this.callBack,
+  }) : super(key: key);
+
+  @override
+  Widget build (BuildContext context) {
+    return Container (
+      height: 500,
+      child: Column (
+        children: <Widget> [
+          ButtonRow(buttons: [
+            Button.big (text: 'AC', color: Button.DARK, callBack: callBack),
+            Button (text: '%', color: Button.DARK, callBack: callBack),
+            Button.operation (text: '/', callBack: callBack),
+          ]),
+
+          const SizedBox(height: 1),
+
+          ButtonRow(buttons: [
+            Button (text: '7', callBack: callBack),
+            Button (text: '8', callBack: callBack),
+            Button (text: '9', callBack: callBack),
+            Button.operation (text: 'x', callBack: callBack),
+          ]),
+
+          const SizedBox(height: 1),
+
+          ButtonRow(buttons: [
+            Button (text: '4', callBack: callBack),
+            Button (text: '5', callBack: callBack),
+            Button (text: '6', callBack: callBack),
+            Button.operation (text: '-', callBack: callBack),
+          ]),
+
+          const SizedBox(height: 1),
+
+          ButtonRow(buttons: [
+            Button (text: '1', callBack: callBack),
+            Button (text: '2', callBack: callBack),
+            Button (text: '3', callBack: callBack),
+            Button.operation (text: '+', callBack: callBack),
+          ]),
+
+          const SizedBox(height: 1),
+
+          ButtonRow(buttons: [
+            Button.big (text: '0', callBack: callBack),
+            Button (text: '.', callBack: callBack),
+            Button.operation (text: '=', callBack: callBack),
+          ]),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/models/memory.dart
@@ -0,0 +1,102 @@
+class Memory {
+  static const operations = [
+    '%',
+    '/',
+    'x',
+    '-',
+    '+',
+    '=',
+  ];
+
+  String _value = '0';
+  final _buffer = [0.0, 0.0];
+  int _bufferIndex = 0;
+  String _operation = '';
+  bool _wipeValue = false;
+  String _lastCommand = '';
+
+  void applyCommand (String command) {
+    if (_isRelacingOperation(command)) {
+      _operation = command;
+      return;
+    }
+
+    if (command == 'AC') {
+      _allClear();
+
+    } else if (operations.contains(command)) {
+      _setOperation(command);
+
+    } else {
+      _addDigit(command);
+    }
+
+    _lastCommand = command;
+  }
+
+  _isRelacingOperation(String command) {
+    return operations.contains(_lastCommand)
+      && operations.contains(command)
+      && _lastCommand != '='
+      && command != '='
+      && _lastCommand != command;
+  }
+
+  _setOperation(String newOperation) {
+    bool isEqualSign = newOperation == '=';
+
+    if (_bufferIndex == 0 && !isEqualSign) {
+      _operation = newOperation;
+      _bufferIndex = 1;
+     _wipeValue = true;
+
+    } else {
+      _buffer[0] = _calculate();
+      _buffer[1] = 0.0;
+
+      _value = _buffer[0].toString().endsWith('.0') ? _buffer[0].toString().split('.0')[0] : _buffer[0].toString();
+
+      _operation = isEqualSign ? '' : newOperation;
+      _bufferIndex = isEqualSign ? 0 : 1;
+    }
+
+    _wipeValue = !isEqualSign;
+  }
+
+  _addDigit(String digit) {
+    final isDot = digit == '.';
+    final wipeValue = (_value == '0' && !isDot) || _wipeValue;
+    final emptyValue = isDot ? '0' : '';
+
+    final currentValue = wipeValue ? emptyValue : _value;
+
+    if (isDot && _value.contains('.') && !wipeValue) return;
+
+    _value = currentValue + digit;
+    _wipeValue = false;
+
+    _buffer[_bufferIndex] = double.tryParse(_value) ?? 0;
+  }
+
+  _allClear () {
+    _value = '0';
+    _buffer.setAll(0, [0.0, 0.0]);
+    _operation = '';
+    _wipeValue = false;
+  }
+
+  _calculate () {
+    switch(_operation) {
+      case '%': return _buffer[0] % _buffer[1];
+      case '/': return _buffer[0] / _buffer[1];
+      case 'x': return _buffer[0] * _buffer[1];
+      case '-': return _buffer[0] - _buffer[1];
+      case '+': return _buffer[0] + _buffer[1];
+      default: return _buffer[0];
+    }
+  }
+
+  String get value {
+    return _value;
+  }
+}
